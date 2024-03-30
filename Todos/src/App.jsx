1972e37344cfc95954e4dc41bc26cff7849f03e6
Jasvant Maddheshiya <Jaswantk8423@gmail.com>
2024-03-30 19:17:34 +0530

    todos

---
+++ Todos/src/App.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import TodoForm  from "./components/TodoForm";
+import  TodoItems  from "./components/TodoItems";
+import { TodoProvider } from "./context/TodoContext";
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <TodoProvider
+      value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <div className="mb-4">
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItems todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
+  );
+}
---
+++ Todos/src/components/index.js
@@ -0,0 +1,3 @@
+import TodoForm from "./TodoForm";
+import TodoItems from "./TodoItems";
+export { TodoForm, TodoItems };
